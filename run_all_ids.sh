c750ef697d72e3a9098b0675d30e3e1c0abcb959
João Pedrosa <ja1antoniolp@gmail.com>
2020-06-04 10:19:01 -0300

    Cleaning the repository

--- run_all_ids.sh
+++
@@ -1,4 +0,0 @@
-for f in $(cat ids)
-do
-    python run.py with sim=499 id_ecg="$f" real='../plots/real'"$f" noise='../plots/noise'"$f" output_name='../results_pkdd/all/' output_name_mean='../results_pkdd/mean/id_'"$f"
-done
